a01bf5c6df4b30a1d2b9c3807323bd36666ea261
Daniel Zwijnenburg <daniel@danielz.nl>
2012-12-19 14:37:13 +0100

    Testing environment

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ source "http://rubygems.org"
 group :development do
   gem "rspec", "~> 2.8.0"
   gem "rdoc", "~> 3.12"
-  gem "bundler", "~> 1.0.0"
+  gem "bundler"
   gem "jeweler", "~> 1.8.4"
-  gem "rcov", ">= 0"
+  gem "webmock", "~> 1.8.0"
+  gem 'vcr'
+  gem "autotest-growl"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,11 +2,19 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'sabnzbd'
-
+require 'webmock/rspec'
+require 'vcr'
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  
+  config.extend VCR::RSpec::Macros
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'fixtures/vcr_cassettes'
+  c.hook_into :webmock
+  c.ignore_localhost = false
+  c.default_cassette_options = { :record => :none }
 end
